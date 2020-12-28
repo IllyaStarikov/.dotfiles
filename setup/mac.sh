7e41f203f3be109ae44b3f8ae7918964833b3d2c
Illya Starikov <starikov@google.com>
2020-12-28 14:00:54 -0800

    Add scripts for setting up the scripts
    Add scripts that will help set up the Unix environment, as best possible
    from scratch.

---
+++ setup/mac.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Brew (and associated packages)
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install python3
+brew install neovim
+brew install ranger
+brew install fzf
